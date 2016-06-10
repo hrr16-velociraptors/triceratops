1e2bcf7736e475a28107f8bccf3297537e0ffe52
Steven Marks <stevenrobertmarks@gmail.com>
2016-06-10 17:17:43 -0500

    closes #14

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ var server = app.listen(port, function(){
 
 var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
-  socket.emit('join','hellow world from client')
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected')
+  })
+  socket.on('chat message',function(msg){
+    console.log('message: '+msg);
+    io.emit('chat message', msg);
+  });
 });
