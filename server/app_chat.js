77fc926402ab65d4d0686cc45034ae6933544ad6
Steven Marks <stevenrobertmarks@gmail.com>
2016-06-11 13:11:14 -0500

    closes #22

--- server/app_chat.js
+++ server/app_chat.js
@@ -1,37 +1,18 @@
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
-var morgan = require('morgan');
 
-
-
-var app = express();
-app.use(morgan('dev'));
-//app.use(express.static(path.resolve(__dirname, '../build')))
-//app.use('/', express.static(path.resolve(__dirname, '../build/index_chat.html')));
 app.use('/', function(req, res){
   res.sendFile(path.resolve(__dirname, '../build/index_chat.html'))
 });
 
-
-var http = require('http').Server(app);
-//var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-
-
-var server = app.listen(port, function(){
-  console.log(`server is now listening on port ${port}`);
-});
-
-var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected')
-  })
-  socket.on('chat message',function(msg){
-    console.log('message: '+msg);
+  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
